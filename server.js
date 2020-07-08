8587b69b32156e0d877029ebc840c0d2e8a7686d
jjjackross <jackrossfilm@outlook.com>
2020-07-08 15:20:52 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+    app = express(),
+    port = 8000,
+    path = require("path"),
+    server = app.listen(port, console.log(`listening on port ${port}`));
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public/dist/public'));
+
+// require('./server/config/database');
+// require('./server/config/routes.js')(app);
+
+app.all("*", (req, res, next) => {
+    res.sendFile(path.resolve("./public/dist/public/index.html"))
+})
